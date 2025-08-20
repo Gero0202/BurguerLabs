9cb12f7f028be47d9df9bcc22d805c6b13a0b4d9
Geronimo Tortosa <geronimo.tortosa@gmail.com>
2025-08-20 16:57:28 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 🚧 Proyecto en desarrollo 🚧
+Este proyecto esta en construcción.
+¡Mantente atento! 👀
---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Kanit, Dancing_Script } from "next/font/google";
+
+const kanit = Kanit({
+  subsets: ["latin"],
+  weight: ["800"]
+})
+
+const edu = Dancing_Script({
+  weight: ["400"]
+})
+
+export default function Home() {
+  return (
+    <>
+      <div className={styles["div-home-principal"]}>
+        <p className={`${styles["titulo-principal"]} ${kanit.className}`}>
+          BURGUER LABS
+        </p>
+
+        <div className={`${styles["div-text-button"]} ${edu.className}`}>
+          <p>Easy to integrate, responsive, and fully customizable.</p>
+          <button>Go!</button>
+        </div>
+      </div>
+    </>
+  );
+}
