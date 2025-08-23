db263d76f9fdaa639c707d55bee962015af15f30
Geronimo Tortosa <geronimo.tortosa@gmail.com>
2025-08-23 14:37:44 -0300

    Axis

---
+++ src/app/components/menus/MenuFour.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import React, { useState } from 'react';
+import styles from "@/app/css/menues/menufour.module.css"
+import { CiMenuBurger } from "react-icons/ci";
+import { IoMdClose } from 'react-icons/io';
+
+
+const MenuAxis = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className={styles["div-general"]}>
+            <button className={styles["button-deploy"]} onClick={toggleMenu}>
+                <CiMenuBurger />
+            </button>
+
+            {isOpen && <div className={styles["overlay"]} onClick={() => setIsOpen(false)} ></div>}
+
+            <nav className={`${styles["nav-menu"]} ${isOpen ? styles["open"] : ""}`}>
+                <button
+                className={styles["close-btn"]}
+                onClick={() => setIsOpen(false)}
+                >
+                    <IoMdClose/>
+                </button>
+                <p className={styles["icono"]}>🍔</p>
+                <ul className={styles["ul-menu"]}>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Acerca de</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
+            </nav>
+        </div >
+    );
+};
+
+export default MenuAxis;
--- src/app/data/menuData.js
+++ src/app/data/menuData.js
@@ -1,6 +1,7 @@
 import MenuOne from "../components/menus/MenuOne"
 import MenuTwo from "../components/menus/MenuTwo"
 import MenuThree from "../components/menus/MenuThree"
+import MenuFour from "../components/menus/MenuFour"
 
 export const menuData = [
   {
@@ -527,10 +528,13 @@ export const menuData = [
               padding: 0;
               box-sizing: border-box;
           }
+
           .div-general{
               width: 100%;
               background-color: rgb(96, 96, 96);
               padding: 10px;
+              display: flex;
+              justify-content: flex-end;
           }
 
           .button-deploy {
@@ -698,6 +702,291 @@ export const menuData = [
                   display: block;
               }
           }
+    `
+  },
+  {
+    id: 4,
+    component: <MenuFour/>,
+    title: "AXIS",
+    reactCode: `
+          'use client'
+
+          import React, { useState } from 'react';
+          import styles from "@/app/page.module.css"
+          import { CiMenuBurger } from "react-icons/ci";
+          import { IoMdClose } from 'react-icons/io';
+
+
+          const MenuAxis = () => {
+            const [isOpen, setIsOpen] = useState(false);
+
+            const toggleMenu = () => {
+              setIsOpen(!isOpen);
+            };
+
+            return (
+              <div className={styles["div-general"]}>
+                <button className={styles["button-deploy"]} onClick={toggleMenu}>
+                  <CiMenuBurger />
+                </button>
+
+                <div
+                  className={\`\${styles["overlay"]} \${isOpen ? styles["show"] : ""}\`}
+                  onClick={() => setIsOpen(false)}
+                ></div>
+
+                <nav className={\`\${styles["nav-menu"]} \${isOpen ? styles["open"] : ""}\`}>
+                  <button
+                    className={styles["close-btn"]}
+                    onClick={() => setIsOpen(false)}
+                  >
+                    <IoMdClose />
+                  </button>
+                  <p className={styles["icono"]}>🍔</p>
+                  <ul className={styles["ul-menu"]}>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Acerca de</a></li>
+                    <li><a href="#">Contacto</a></li>
+                  </ul>
+                </nav>
+              </div >
+            );
+          };
+
+          export default MenuAxis;
+    `,
+    javascripCode: `
+          const menuToggle = document.getElementById("menu-toggle");
+          const menu = document.getElementById("menu");
+          const overlay = document.getElementById("menu-overlay");
+          const menuClose = document.getElementById("menu-close");
+
+          menuToggle.addEventListener("click", () => {
+            menu.classList.toggle("open")
+            overlay.classList.toggle("show")
+          });
+
+          overlay.addEventListener("click", () => {
+            menu.classList.remove("open")
+            overlay.classList.remove("show")
+          });
+
+
+          menuClose.addEventListener("click", () => {
+            menu.classList.remove("open")
+            overlay.classList.remove("show")
+          });
+    `,
+    htmlCode: `
+          <!DOCTYPE html>
+          <html lang="es">
+
+          <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Menu Axis</title>
+            <link rel="stylesheet" href="styles.css">
+          </head>
+
+          <body>
+
+            <div class="div-general">
+
+              <button class="button-deploy" id="menu-toggle">
+                &#9776;
+              </button>
+
+              <div class="overlay" id="menu-overlay"></div>
+
+              <nav class="nav-menu" id="menu">
+                <button class="close-btn" id="menu-close">
+                  &times;
+                </button>
+                <p class="icono">🍔</p>
+                <ul class="ul-menu">
+                  <li><a href="#">Inicio</a></li>
+                  <li><a href="#">Acerca de</a></li>
+                  <li><a href="#">Contacto</a></li>
+                </ul>
+              </nav>
+            </div>
+
+            <script src="index.js"></script>
+          </body>
+
+          </html>
+    `,
+    cssCode: `
+           *{
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+            }
+                
+            .div-general {
+                width: 100%;
+                background-color: rgb(96, 96, 96);
+                border-radius: 0px;
+                padding: 10px;
+            }
+
+            .button-deploy {
+                background: transparent;
+                border: none;
+                cursor: pointer;
+                padding: 0.6rem;
+                border-radius: 10px;
+                transition: all 0.3s ease;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                font-size: 1.8rem;
+                color: #f5f5f5;
+                position: relative;
+                z-index: 1000;
+            }
+
+            .button-deploy:hover {
+                background: rgba(255, 255, 255, 0.08);
+                transform: scale(1.08);
+                color: #ffffff;
+            }
+
+            .button-deploy:active {
+                transform: scale(0.95);
+                background: rgba(255, 255, 255, 0.2);
+            }
+
+            .nav-menu {
+                position: fixed; 
+                top: 0;
+                right: 0;
+                width: 250px;
+                height: 100%;
+                background: black;
+                transform: translateX(100%);
+                opacity: 0;
+                transition: all 0.5s ease;
+                z-index: 1000;
+                display: flex;
+                flex-direction: column;
+            }
+
+            .nav-menu.open {
+                transform: translateX(0);
+                opacity: 1;
+
+            }
+
+            .ul-menu {
+                display: flex;
+                align-items: center;
+                flex-direction: column;
+                height: 100%;
+                gap: 100px;
+                margin-top: 150%;
+            }
+
+            .ul-menu li {
+                list-style-type: none;
+                color: rgb(255, 255, 255);
+                font-size: 1em;
+                font-family: sans-serif;
+            }
+
+            .ul-menu li a {
+                text-decoration: none;
+                color: inherit;
+            }
+
+            .icono {
+                font-size: 2.8em;
+                text-align: center;
+                margin-top: 10%;
+            }
+
+            .overlay {
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                background-color: rgba(0, 0, 0, 0.5);
+                display: none;
+                transition: opacity 0.3s ease;
+                z-index: 998;
+            }
+
+            .overlay.show {
+                opacity: 1;
+                pointer-events: all;
+                display: block;
+            }
+
+            .close-btn {
+                position: absolute;
+                top: 15px;
+                right: 20px;
+                background: transparent;
+                border: none;
+                color: white;
+                font-size: 2rem;
+                cursor: pointer;
+                display: none;
+                z-index: 1001;
+            }
+
+            @media (max-width: 800px) {
+                .div-general {
+                    position: relative;
+                    border-radius: 0px;
+                }
+
+                .nav-menu {
+                    position: absolute;
+                    top: 100%;
+                    left: 0;
+                    right: 0;
+                    width: 100%;
+                    margin: 0 auto;
+                    height: auto;
+                    background-color: #303030;
+                    border-radius: 0 0 12px 12px;
+                    transform: translateY(-20px);
+                    opacity: 0;
+                    transition: all 0.3s ease;
+                    padding: 20px 0;
+                    z-index: 1000;
+                }
+
+                .nav-menu.open {
+                    transform: translateY(0);
+                    opacity: 1;
+                }
+
+                .ul-menu {
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
+                    gap: 18px;
+                    margin: 0;
+                    height: auto;
+                    padding: 0;
+                }
+
+                .ul-menu li {
+                    font-size: 1.2em;
+                }
+
+                .icono {
+                    margin-top: 0;
+                    font-size: 2em;
+                }
+
+                .overlay {
+                    display: none;
+                }
+            }
     `
   }
 ]
